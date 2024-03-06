5caa568c0e0cf96142c46607571512a4c769d5e9
nerogamer848 <b.carrasco.cruz848@gmail.com>
2024-03-05 21:20:07 -0400

    Pepe

--- Program.cs
+++ Program.cs
@@ -1,12 +1,1 @@
-﻿namespace proyecto
-{
-    class progra3{
-        public static void Main(string[] args)
-        {
-            int mEent = 12345;
-            int mDiv = 12345 % 10;
-         Console.Writeline(mEent);
-         Console.Writeline(mDiv);
-        }
-    }
-}
+﻿Console.WriteLine("chipi chipi chapa chapa")
